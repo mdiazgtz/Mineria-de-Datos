8bf8bff03d7752ad7f735902e3bc18a4d120aaa1
mdiazgtz <78995651+mdiazgtz@users.noreply.github.com>
2021-03-20 10:45:32 -0600

    Create DataAdquisition.py

---
+++ DataAdquisition.py
@@ -0,0 +1,23 @@
+#Data Adquisition
+import requests
+import io
+from bs4 import BeautifulSoup
+import pandas as pd
+from tabulate import tabulate
+from typing import Tuple, List
+import re
+from datetime import datetime
+
+def get_soup(url: str) -> BeautifulSoup:
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+def get_csv_from_url(url:str) -> pd.DataFrame:
+    s=requests.get(url).content
+    return pd.read_csv(io.StringIO(s.decode('utf-8')))
+
+def print_tabulate(df: pd.DataFrame):
+    print(tabulate(df, headers=df.columns, tablefmt='orgtbl'))
+
+df = pd.read_csv("C:\\Users\\MIKID\\Documents\\Michelle - Facultad de Ciencias Físico Matemáticas\\7TO SEMESTRE\\MINERÍA DE DATOS\\netflix_titles.csv")
+print_tabulate(df)
